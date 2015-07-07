bb8984932c6b97cdb02dab13114e57e60a89c435
Julio Rincon <julio.rincon@transport.nsw.gov.au>
2015-07-07 17:18:04 +1000

    evilmatchit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -434,3 +434,26 @@
 ;; =========== line numbers =====================
 (unless (package-installed-p 'linum)
   (package-install 'linum))
+
+;; ========= delete file ====================
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+(global-set-key (kbd "C-c D")  'delete-file-and-buffer)
+
+;; ================== eveil-matchit ============
+(unless (package-installed-p 'evil-matchit)
+  (package-install 'evil)
+  (package-install 'evil-matchit)
+  )
+(evil-mode 1)
+(global-evil-matchit-mode 1)
